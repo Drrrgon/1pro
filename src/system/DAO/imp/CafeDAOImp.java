28849fa19db7ebed7d3fc5c1b877f1c3cd059060
Dragon <heartofbeholder@gmail.com>
2018-05-12 00:14:56 +0900

    Primary DAO

---
+++ src/system/DAO/imp/CafeDAOImp.java
@@ -0,0 +1,36 @@
+package system.DAO.imp;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import system.DAO.CafeDAO;
+import system.config.CafeConfig;
+import vo.MemberVo;
+import vo.MenuVo;
+
+public class CafeDAOImp implements CafeDAO {
+	private SqlSession sqlSession;
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public CafeDAOImp() {
+		sqlSessionFactory = CafeConfig.getSqlSessionFactory();
+		sqlSession = sqlSessionFactory.openSession();
+	}
+	@Override
+	public List<MemberVo> getAllMenu() {
+		return sqlSession.selectList("Menu.getAllMenu");
+	}
+
+	@Override
+	public void deleteMenu(MenuVo menu) {
+		sqlSession.delete("Menu.deleteMenu",menu);		
+	}
+
+	@Override
+	public void insertMenu(MenuVo menu) {
+		sqlSession.insert("Menu.insertMenu", menu);		
+	}
+
+}
