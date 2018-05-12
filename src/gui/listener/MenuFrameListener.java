12ba70cc137f80e5ed283004dd203cb2fb350af0
Dragon <heartofbeholder@gmail.com>
2018-05-12 12:36:18 +0900

    Doing Menu + Gui

--- src/gui/listener/MenuFrameListener.java
+++ src/gui/listener/MenuFrameListener.java
@@ -1,11 +1,13 @@
 package gui.listener;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.ListSelectionEvent;
 
 import gui.MenuFrame;
 import system.DAO.imp.CafeDAOImp;
@@ -15,24 +17,32 @@ public class MenuFrameListener implements ActionListener{
 	MenuFrame menuFrame;
 	CafeDAOImp cafeDAOImp; 
 	
+	
 	public MenuFrameListener(MenuFrame menuFrame){
 		this.menuFrame = menuFrame;
 		cafeDAOImp = new CafeDAOImp();
+		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-		case "print":
-						
-			List<MenuVo> list = cafeDAOImp.getAllMenu();
-			String temp[]= new String[list.size()+1];
-			for(int i = 0 ; i < list.size() ; i ++) {
-				temp[i] = list.get(i).toString();			
-			}
-			menuFrame.list.setListData(temp);
-			menuFrame.list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		case "reg":
+			MenuVo temp= new MenuVo();
+			temp.setmName(menuFrame.txtMenuname.getText());
+			temp.setmPrice(Integer.parseInt(menuFrame.txtMenuprice.getText()));
+			temp.setmOriginalPrice(Integer.parseInt(menuFrame.originalPrice.getText()));			
+			if(cafeDAOImp.insertMenu(temp) == 0) {
+				menuFrame.resultField.setText("메뉴 추가 실패");
+			}else {
+				menuFrame.resultField.setText("메뉴 추가 성공");
+				menuFrame.modelMenuList.addElement(temp.toString());
+			}					
 			break;
-
+			
+		case "del":
+			MenuVo temp2= new MenuVo();
+			menuFrame.menuList.getSelectedIndex();
+			break;
 		default:
 			break;
 		}
